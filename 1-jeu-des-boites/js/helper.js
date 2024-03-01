e2c248233b9b5606c8a84cb046a8e48847537092
Nassim BERABHA <nassim.berabha@stagiaire-elan.fr>
2024-03-01 16:59:09 +0100

    Ajout morpion

--- 1-jeu-des-boites/js/helper.js
+++ 1-jeu-des-boites/js/helper.js
@@ -4,6 +4,6 @@ let timer = 0 // initialisation du timer pour utilisé dans la fonction startTim
 let secondes = 0 // initialisation des secondes
 let milisecondes = 0 //initialisation des milisecondes
 let nb = 1 // initialisation du score (ici c'est la boite voulue) pour controler l'état du jeu
-let boxNumber = 9 /*nombre de boites */
+let boxNumber = 3 /*nombre de boites */
 let refreshIntervalId // permet de refresh l'intervalle du timer
 let bestTime = 99999999999999
